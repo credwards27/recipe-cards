c9ba75b92a801d0b736f45d5614d9ffcedc9952f
credwards27 <credwards27@users.noreply.github.com>
2021-03-19 23:36:57 -0400

    Added RecipeList and Recipe objects and intial test data loading logic

--- src/js/windows/browser.js
+++ src/js/windows/browser.js
@@ -4,7 +4,16 @@
 */
 
 // Dependencies.
-const onReady = require("app/front/utils/ready.js");
+const onReady = require("app/front/utils/ready.js"),
+    RecipeList = require("app/front/browser/recipe-list.js");
 
 onReady(() => {
+    let data = document.getElementById("test-data"),
+        recipes;
+    
+    data = JSON.parse(data.innerText.trim());
+    
+    recipes = new RecipeList(data.recipes);
+    
+    window.recipes = recipes;
 });
