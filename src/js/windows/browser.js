c37236bc22792d5a372310d7e32a2511e3eb996e
credwards27 <credwards27@users.noreply.github.com>
2021-03-29 00:24:10 -0400

    Removed test data load and unused dependencies from browser view entry point

--- src/js/windows/browser.js
+++ src/js/windows/browser.js
@@ -6,15 +6,9 @@
 // Dependencies.
 const ReactDOM = require("react-dom"),
     onReady = require("app/front/utils/ready.js"),
-    App = require("app/front/browser/components/app.js"),
-    BrowserForm = require("app/front/browser/components/browser-form.js"),
-    RecipeList = require("app/front/browser/components/recipe-list.js");
+    App = require("app/front/browser/components/app.js");
 
 onReady(document, () => {
-    let data = document.getElementById("test-data");
-    
-    data = JSON.parse(data.innerText.trim());
-    
     let app = ReactDOM.render(
         <App />,
         document.getElementById("app")
