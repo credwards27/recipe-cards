9b581ee061577ec2805370cc0dd6e1949710c082
credwards27 <credwards27@users.noreply.github.com>
2021-04-03 12:57:32 -0400

    Changed (output view):
    - Added test data to HTML file (not yet fully used)
    - Moved existing recipe card DOM to initial output App component
    - Moved existing scale logic to App component

--- src/js/windows/output.js
+++ src/js/windows/output.js
@@ -8,67 +8,21 @@ const ReactDOM = require("react-dom"),
     onReady = require("app/front/utils/ready.js"),
     App = require("app/front/output/components/app.js");
 
-// Recipe card dimension properties.
-const CARD_DIMENSIONS = {
-    max: 1440,
-    min: 320,
-    actual: 0,
-    maxScale: 1,
-    minScale: 0
-};
-
-let htmlElem = null,
-    
-    cardWrapper = null;
-
-/* Updates recipe card scale.
-*/
-function updateScale() {
-    let { clientWidth: vWidth } = htmlElem,
-        { actual, maxScale, minScale } = CARD_DIMENSIONS,
-        scale = vWidth / actual,
-        origin = vWidth < actual ? "top left " : "top center";
-    
-    // Clamp
-    switch (true) {
-        case scale > maxScale:
-            scale = maxScale;
-            break;
-        
-        case scale < minScale:
-            scale = minScale;
-            break;
-    }
-    
-    cardWrapper.style.transformOrigin = origin;
-    cardWrapper.style.transform = `scale(${scale})`;
-}
-
 onReady(document, () => {
-    /*let app = ReactDOM.render(
-        <App />,
+    let recipe = document.getElementById("test-data"),
+        htmlElem = document.getElementsByTagName("html")[0];
+    
+    recipe = JSON.parse(recipe.text);
+    
+    app = ReactDOM.render(
+        <App
+            htmlElem={htmlElem}
+            recipe={recipe}
+            max={1440}
+            min={320}
+        />,
         document.getElementById("app")
     );
     
-    window.app = app;*/
-    
-    htmlElem = document.getElementsByTagName("html")[0];
-    cardWrapper = document.getElementById("recipe-cards");
-    
-    let templateCard = document.getElementsByClassName("recipe-card")[0],
-        dims = CARD_DIMENSIONS,
-        cardWidth = parseInt(
-            getComputedStyle(templateCard).width.replace(/\D+/g, ""),
-            10
-        );
-    
-    dims.actual = cardWidth;
-    dims.maxScale = dims.max / cardWidth;
-    dims.minScale = dims.min / cardWidth;
-    
-    window.addEventListener("resize", (e) => {
-        updateScale();
-    });
-    
-    updateScale();
+    window.app = app;
 });
