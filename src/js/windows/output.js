23237e34aa1651a4fffb42d0643a7fd8de0c0fa9
credwards27 <credwards27@users.noreply.github.com>
2021-04-07 12:47:32 -0400

    Removed test data load step from output view

--- src/js/windows/output.js
+++ src/js/windows/output.js
@@ -9,15 +9,11 @@ const ReactDOM = require("react-dom"),
     App = require("app/front/output/components/app.js");
 
 onReady(document, () => {
-    let recipe = document.getElementById("test-data"),
-        htmlElem = document.getElementsByTagName("html")[0];
-    
-    recipe = JSON.parse(recipe.text.replace(/&lt;/g, "<"));
+    let htmlElem = document.getElementsByTagName("html")[0];
     
     app = ReactDOM.render(
         <App
             htmlElem={htmlElem}
-            recipe={recipe}
             max={1440}
             min={320}
         />,
