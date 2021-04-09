580591593c6737983c791a38e9104d04905d2726
credwards27 <credwards27@users.noreply.github.com>
2021-04-09 14:29:12 -0400

    Browser and output views now synchronize with the currently selected recipe if the window is closed and reopened

--- src/js/windows/browser.js
+++ src/js/windows/browser.js
@@ -8,9 +8,14 @@ const ReactDOM = require("react-dom"),
     onReady = require("app/front/utils/ready.js"),
     App = require("app/front/browser/components/app.js");
 
-onReady(document, () => {
-    let app = ReactDOM.render(
-        <App />,
+onReady(document, async () => {
+    let selected = await ipc.invoke("get-selected-recipe"),
+        app;
+    
+    app = ReactDOM.render(
+        <App
+            selected={selected?.id}
+        />,
         document.getElementById("app")
     );
     
--- src/js/windows/output.js
+++ src/js/windows/output.js
@@ -8,11 +8,14 @@ const ReactDOM = require("react-dom"),
     onReady = require("app/front/utils/ready.js"),
     App = require("app/front/output/components/app.js");
 
-onReady(document, () => {
-    let htmlElem = document.getElementsByTagName("html")[0];
+onReady(document, async () => {
+    let htmlElem = document.getElementsByTagName("html")[0],
+        selected = await ipc.invoke("get-selected-recipe"),
+        app;
     
     app = ReactDOM.render(
         <App
+            recipe={selected}
             htmlElem={htmlElem}
             max={1440}
             min={320}
